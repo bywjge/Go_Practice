889b30f75abc1fc36a7a56f767419ee5b3ae4de0
OXL <oxl02@qq.com>
2022-05-31 20:32:10 +0800

    20220531

---
+++ 05-switch/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	a := 2
+	switch a {
+	// 默认不需要加break
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	case 4, 5:
+		fmt.Println("four or five")
+	default:
+		fmt.Println("other")
+	}
+	// 更加强大 支持多种类型
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+}
