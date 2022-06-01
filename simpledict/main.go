751546a51e5ce538025471d63ee0c645a136e489
OXL <oxl02@qq.com>
2022-06-01 23:47:57 +0800

    20220601

--- simpledict/main.go
+++ simpledict/main.go
@@ -106,9 +106,10 @@ func main() {
 	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, `usage: simpleDict WORD
 example: simpleDict hello
-		`)
+			`)
 		os.Exit(1)
 	}
 	word := os.Args[1]
 	query(word)
+	//query("apple")
 }
